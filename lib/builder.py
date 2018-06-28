c5bd9e2818c9f61c1cdc81b51080c6db528a2be8
Sjoerd Adema <s.j.adema@iwink.nl>
2018-06-28 22:06:45 +0200

    Changed the way dependencies are resolved so remote dependencies depend on local dependencies

--- lib/builder.py
+++ lib/builder.py
@@ -1,9 +1,9 @@
 import glob
 import logging
 import subprocess
-import sys
+from typing import Dict, List
 
-from lib.dependency import Node, Resolver
+from lib.dependency import Node, Resolver, Graph
 from lib.image import Image
 
 
@@ -12,62 +12,96 @@ class Builder:
     def __init__(self):
 
         self.images = {}
+        self.graph = None
+
         self.local_dependencies = []
-        self.external_dependencies = []
+        self.remote_dependencies = []
 
     def run(self):
         """ Runs methods to build all images. """
-        self.read_manifest()
         self.index_images()
         self.resolve_dependencies()
+        # self.resolve_dependency('kirra-php:7.1')
+
         self.pull_images()
-        self.build_images()
+        # self.build_images()
 
     def index_images(self) -> None:
-        """ Index the Images found in the current directory. """
+        """
+        Index the images found in the current directory and build their dependency graph.
+        """
 
         for dockerfile in glob.glob('containers/*/Dockerfile'):
             image = Image(dockerfile)
             image.index()
             self.images[image.name] = image
 
+        self.graph = self._build_dependency_graph()
+
     def resolve_dependencies(self) -> None:
-        """ Resolve the dependencies of the indexed Images. """
+        """
+        Resolve the dependencies of all indexed images.
+        :return: None.
+        """
 
-        nodes = {image.name: Node(image.name) for image in self.images.values()}
+        self._build_dependencies(Resolver(self.graph.nodes.values()).resolve_dependencies())
 
-        for image in self.images.values():
+    def resolve_dependency(self, name: str) -> None:
+        """
+        Resolve dependencies for a single indexed image and return them.
+        :param name: The name of the image to resolve the dependencies for.
+        :return:
+        """
 
-            for dep in image.dependencies:
+        self._build_dependencies(Resolver([self.graph.locals[name]]).resolve_dependencies())
 
-                if dep in self.images:
-                    nodes[image.name].add_edge(nodes[dep])
+    def _build_dependency_graph(self) -> 'Graph':
+        """
+        Builds a dependency graph for the images. Starts by creating a node for every image and dependency and then
+        adding the edges.
+        :return: An initialized dependency graph.
+        """
 
-                elif dep not in self.external_dependencies:
-                    self.external_dependencies.append(dep)
+        images = self.images.values()
 
-        resolver = Resolver(nodes.values())
-        dependencies = resolver.resolve_dependencies()
+        graph = Graph()
+        for image in images:
+            graph.add_local(Node(image.name))
 
-        self.local_dependencies = dependencies
+            for dependency in image.dependencies:
+                if dependency not in graph.nodes:
+                    if dependency in self.images:
+                        graph.add_local(Node(dependency))
+                    else:
+                        graph.add_remote(Node(dependency))
 
-        logging.debug("External dependencies found: {:s}".format(str(self.external_dependencies)))
+        for image in images:
+            for dependency in image.dependencies:
+                graph.nodes[image.name].add_edge(graph.nodes[dependency])
+
+        logging.debug("Dependency graph (local nodes): {:s}".format(str(list(graph.locals.keys()))))
+        logging.debug("Dependency graph (remote nodes): {:s}".format(str(list(graph.remotes.keys()))))
+
+        return graph
+
+    def _build_dependencies(self, dependencies: List['Node']) -> None:
+        for dependency in dependencies:
+            if dependency.name in self.graph.locals:
+                self.local_dependencies.append(dependency.name)
+            else:
+                self.remote_dependencies.append(dependency.name)
 
     def build_images(self):
         """ Build the indexed local Images in order of dependencies, lowest number of dependencies
         first. """
 
-        for dep in self.local_dependencies:
-            self.images[dep.name].build()
-
-    def read_manifest(self):
-        #self.manifest = json.load(open(manifest, 'r'))
-        pass
+        for dependency in self.local_dependencies:
+            self.images[dependency.name].build()
 
     def pull_images(self):
-        """ Pull external dependencies. """
+        """ Pull remote dependencies. """
 
-        for image in self.external_dependencies:
+        for image in self.remote_dependencies:
             logging.debug("Pulling image {:s}".format(image))
 
             command = "docker pull {:s}".format(image).split(" ")
--- lib/dependency.py
+++ lib/dependency.py
@@ -1,13 +1,14 @@
 import logging
+from typing import List
 
 
 class Node:
-    def __init__(self, name):
+    def __init__(self, name: str):
         self.name = name
         self.edges = []
 
-    def add_edge(self, dependency):
-        self.edges.append(dependency)
+    def add_edge(self, node: 'Node') -> None:
+        self.edges.append(node)
 
     def __repr__(self):
         return self.name
@@ -17,7 +18,14 @@ class Resolver:
     def __init__(self, graph):
         self.graph = graph
 
-    def resolve_dependency(self, node, resolved, unresolved):
+    def resolve_dependency(self, node: 'Node', resolved: List['Node'], unresolved: List['Node']) -> None:
+        """
+        Resolves the dependencies for a single node.
+        :param node: The node to resolve the dependencies for.
+        :param resolved: A list of resolved dependencies.
+        :param unresolved: A list of unresolved dependencies.
+        :return: None
+        """
 
         if node in resolved:
             return
@@ -35,7 +43,11 @@ class Resolver:
         resolved.append(node)
         unresolved.remove(node)
 
-    def resolve_dependencies(self):
+    def resolve_dependencies(self) -> List['Node']:
+        """
+        Resolves the dependencies for the graph.
+        :return:
+        """
 
         resolved = []
         for node in self.graph:
@@ -43,3 +55,31 @@ class Resolver:
 
         logging.debug("Resolved dependency order {:s}".format(str(resolved)))
         return resolved
+
+
+class Graph:
+    def __init__(self):
+        self.locals = {}
+        self.remotes = {}
+        self.nodes = {}
+
+    def add_local(self, node: 'Node') -> None:
+        """
+        Adds a node to the local nodes and the graph.
+        :param node: The node to add.
+        :return: None.
+        """
+
+        self.nodes[node.name] = node
+        self.locals[node.name] = node
+
+    def add_remote(self, node: 'Node') -> None:
+        """
+        Adds a node to the remote nodes and the graph.
+        :param node: The node to add.
+        :return: None.
+        """
+
+        self.nodes[node.name] = node
+        self.remotes[node.name] = node
+
